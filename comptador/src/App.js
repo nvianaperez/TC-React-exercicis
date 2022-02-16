2f7e5fe3a7021c5a21cb62e44cba539c488c9960
nviana <nvianaperez@gmail.com>
2022-02-16 17:30:57 +0100

    comptador exercise

--- comptador/src/App.js
+++ comptador/src/App.js
@@ -4,15 +4,29 @@ import "./App.css";
 function App() {
   const [display, setDisplay] = useState(0);
 
+  const controlOperacio = (operacio) => {
+    if (display + parseInt(operacio) < 0) {
+      console.log(display); // display és el valor anterior, falta sumar-li la operacio, que com que és un string ho haig de passar a número
+      return false;
+    } else if (display + parseInt(operacio) > 10) {
+      console.log(display);
+      return false;
+    } else return true;
+  };
+
   const entraOperacio = (operacio) => {
-    if (operacio === "+1") {
-      setDisplay(display + 1);
-    }
-    if (operacio === "-1") {
-      setDisplay(display - 1);
-    }
-    if (operacio === "+5") {
-      setDisplay(display + 5);
+    if (!controlOperacio(operacio)) {
+      setDisplay("error");
+    } else {
+      if (operacio === "+1") {
+        setDisplay(display + 1);
+      }
+      if (operacio === "-1") {
+        setDisplay(display - 1);
+      }
+      if (operacio === "+5") {
+        setDisplay(display + 5);
+      }
     }
   };
 
