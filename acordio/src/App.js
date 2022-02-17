a681f1ba1b1964017cee59f2e21e05f67eed0c7f
nviana <nvianaperez@gmail.com>
2022-02-17 18:02:43 +0100

    acordio exercise

--- acordio/src/App.js
+++ acordio/src/App.js
@@ -4,17 +4,17 @@ import "./App.css";
 
 const acordioArr = [
   { id: 1, titol: "Títol 1", text: "Contingut 1" },
-  { id: 1, titol: "Títol 2", text: "Contingut 2" },
-  { id: 1, titol: "Títol 3", text: "Contingut 3" },
-  { id: 1, titol: "Títol 4", text: "Contingut 4" },
+  { id: 2, titol: "Títol 2", text: "Contingut 2" },
+  { id: 3, titol: "Títol 3", text: "Contingut 3" },
+  { id: 4, titol: "Títol 4", text: "Contingut 4" },
 ];
 
 function App() {
-  const [textDisplay, setTextDisplay] = useState("Text");
   const [textClicked, setTextClicked] = useState("null");
+  const [textDisplay, setTextDisplay] = useState("Text-none");
 
-  const newDisplay = (e) => {
-    setTextClicked(e.target.text); //<h1 class="Titol">Títol 1</h1>
+  const newDisplay = (element) => {
+    setTextClicked(element);
     if (textDisplay === "Text") {
       setTextDisplay("Text-none");
     } else {
@@ -26,14 +26,10 @@ function App() {
     <div className="Container">
       {acordioArr.map((element) => (
         <div key={element.id}>
-          <h1
-            // className={element === textClicked ? textDisplay : "Text"}
-            className="Text"
-            onClick={newDisplay}
-          >
+          <h1 className="Text" onClick={() => newDisplay(element)}>
             {element.titol}
           </h1>
-          <h5 className={element.text === textClicked ? textDisplay : "Text"}>
+          <h5 className={element === textClicked ? textDisplay : "Text-none"}>
             {element.text}
           </h5>
         </div>
